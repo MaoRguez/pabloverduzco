a123722cb0b03481966288df9307ef1d493d9cfa
Pablo Verduzco <pabloverduzcos@gmail.com>
2021-03-22 20:17:54 -0600

    add: set app fonts from google fonts cdn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
+import Head from 'next/head';
 import { AppProps } from 'next/app';
 
+import 'styles/globals.css';
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&family=Work+Sans:wght@400;700&display=swap'
+          rel='print'
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          onLoad="this.media='all'"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
